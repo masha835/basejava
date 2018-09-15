3e83e789e2a3f1d09819a1a4f20583b1f6138bb8
PHP <31829822+masha835@users.noreply.github.com>
2018-09-15 15:16:04 +0200

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
-/**
- * Array based storage for Resumes
- */
-public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+import java.util.Arrays; 
 
-    void clear() {
-    }
+/** 
+* Array based storage for Resumes 
+*/ 
+public class ArrayStorage { 
+private Resume[] storage = new Resume[10000]; 
+//по ООП мы делаем приватным то,что мы не хотим, что мог бы изменять пользователь(инкапсуляция) или закрытие реализации 
+private int counter = 0; 
 
-    void save(Resume r) {
-    }
 
-    Resume get(String uuid) {
-        return null;
-    }
 
-    void delete(String uuid) {
-    }
+public void clear() { 
+Arrays.fill(storage, 0, counter, null); 
+counter = 0; 
+} 
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+public void save(Resume r) { 
+storage[counter] = r; 
+counter++; 
+} 
 
-    int size() {
-        return 0;
-    }
+public Resume get(String uuid) { 
+for (int i = 0; i < counter; i++) { 
+if (storage[i].uuid.equals(uuid)) { 
+return storage[i]; 
+} 
+} 
+return null; 
+} 
+
+public void delete(String uuid) { 
+for (int i = 0; i < counter; i++) { 
+if (storage[i].uuid.equals(uuid)) { 
+counter--; 
+storage[counter]=null; 
+break; 
+} 
+} 
+
+} 
+
+public Resume[] getAll() { 
+return Arrays.copyOf(storage, counter); 
+} 
+
+public int size() { 
+return counter; 
+} 
 }
