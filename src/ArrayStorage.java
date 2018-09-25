52204643205f700ac2f5ee1f4e766ca89c4be196
PHP <31829822+masha835@users.noreply.github.com>
2018-09-25 22:13:28 +0200

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,50 +1,70 @@
-import java.util.Arrays; 
+import java.util.Arrays;
 
-/** 
-* Array based storage for Resumes 
-*/ 
-public class ArrayStorage { 
-private Resume[] storage = new Resume[10000]; 
-//по ООП мы делаем приватным то,что мы не хотим, что мог бы изменять пользователь(инкапсуляция) или закрытие реализации 
-private int counter = 0; 
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    //по ООП мы делаем приватным то,что мы не хотим, что мог бы изменять пользователь(инкапсуляция) или закрытие реализации
+    private int counter = 0;
+
+
+    public void clear() {
+        Arrays.fill(storage, 0, counter, null);
+        counter = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.uuid);
+        storage[index] = r;
+
+        System.out.println("Резюме" + r.uuid + "уже существует");
 
 
 
-public void clear() { 
-Arrays.fill(storage, 0, counter, null); 
-counter = 0; 
-} 
 
-public void save(Resume r) { 
-storage[counter] = r; 
-counter++; 
-} 
+    }
 
-public Resume get(String uuid) { 
-for (int i = 0; i < counter; i++) { 
-if (storage[i].uuid.equals(uuid)) { 
-return storage[i]; 
-} 
-} 
-return null; 
-} 
+    public void save(Resume r) {
+        storage[counter] = r;
+        counter++;
+    }
 
-public void delete(String uuid) { 
-for (int i = 0; i < counter; i++) { 
-if (storage[i].uuid.equals(uuid)) { 
-counter--; 
-storage[counter]=null; 
-break; 
-} 
-} 
+    public int getIndex(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if ((storage[i].uuid).equals(uuid)) {
+                return i;
 
-} 
 
-public Resume[] getAll() { 
-return Arrays.copyOf(storage, counter); 
-} 
+            }
 
-public int size() { 
-return counter; 
-} 
+        }
+        return -1;
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+
+        return storage[index];
+    }
+
+
+
+    public void delete(String uuid) {
+
+
+        counter--;
+        storage[counter] = null;
+
+
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, counter);
+    }
+
+    public int size() {
+        return counter;
+    }
 }
+
