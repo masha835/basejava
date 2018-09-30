c95f7fe1ee73333c822a910bade0452774a4ec47
PHP <31829822+masha835@users.noreply.github.com>
2018-09-30 15:59:56 +0200

    Importantedits2lesson
    Были изменены полностью методы update,save, get, delete - их логика теперь совершенно другая
    У метода getIndex изменен модификатор доступа на private
    Убрала лишние пробелы, а также отформатировала код с помощью сочетания горячих клавиш ctrl+alt+l

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,10 +4,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10_000];
     //по ООП мы делаем приватным то,что мы не хотим, что мог бы изменять пользователь(инкапсуляция) или закрытие реализации
     private int counter = 0;
-
 
     public void clear() {
         Arrays.fill(storage, 0, counter, null);
@@ -16,47 +15,68 @@ public class ArrayStorage {
 
     public void update(Resume r) {
         int index = getIndex(r.uuid);
-        storage[index] = r;
+        if (index == -1) {
+            System.out.println("Не существует");
 
-        System.out.println("Резюме" + r.uuid + "уже существует");
-
-
-
-
+        } else {
+            storage[index] = r;
+        }
+        //  Resume resume = storage[index] = null;
+        // else(storage[index] = null){
+        //  System.out.println("Не существует данных для проверки");
+        //}
     }
+
 
     public void save(Resume r) {
-        storage[counter] = r;
-        counter++;
+
+        if (getIndex(r.uuid) != -1) {
+            System.out.println("Резюме существует");
+
+        } else if (counter >= storage.length) {
+            System.out.println("Места больше нет");
+        } else {
+            storage[counter] = r;
+            counter++;
+        }
+
     }
 
-    public int getIndex(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < counter; i++) {
             if ((storage[i].uuid).equals(uuid)) {
                 return i;
 
-
+// метод getIndex сравнивает переданное uuid каждого элемента массива storage и возвращает целочисленный индекс найденного элемента
             }
-
         }
         return -1;
     }
 
     public Resume get(String uuid) {
-        int index = getIndex(uuid);
 
-        return storage[index];
+        int index = getIndex(uuid);
+        if (index != -1) {
+            return storage[index];
+            // int index = getIndex(uuid);
+            //  return storage[index];
+        } else {
+            System.out.println("Ошибка");
+            return null;
+        }
     }
 
-
-
     public void delete(String uuid) {
-
-
-        counter--;
-        storage[counter] = null;
-
-
+        int index = getIndex(uuid);
+        if (index != -1) {
+            int res = counter - 1;
+            storage[index] = null;
+        }
+        //counter--;
+        //storage[counter] = null;
+        else {
+            System.out.println("Ошибка");
+        }
     }
 
     public Resume[] getAll() {
