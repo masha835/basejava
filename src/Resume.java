6a6cfe5ebf2a1782e1676b707f45c538d1c78f5e
PHP <31829822+masha835@users.noreply.github.com>
2018-10-02 19:26:01 +0200

    Resume.java
    Добавила геттеры и сеттеры

--- src/Resume.java
+++ src/Resume.java
@@ -10,4 +10,12 @@ public class Resume {
     public String toString() {
         return uuid;
     }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
