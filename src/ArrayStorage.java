02179c1bf36006d174ca62517f954cd0cf4f926b
PHP <31829822+masha835@users.noreply.github.com>
2018-10-02 18:28:11 +0200

    errordelete
    Исправила метод delete, прописала более точно S.out ошибки у методов detele и get, удалила лишние пустые строки и комментарии, оставила только один(он пока что для меня важен,как подсказка)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10_000];
-    //по ООП мы делаем приватным то,что мы не хотим, что мог бы изменять пользователь(инкапсуляция) или закрытие реализации
     private int counter = 0;
 
     public void clear() {
@@ -21,13 +20,9 @@ public class ArrayStorage {
         } else {
             storage[index] = r;
         }
-        //  Resume resume = storage[index] = null;
-        // else(storage[index] = null){
-        //  System.out.println("Не существует данных для проверки");
-        //}
+
     }
-
-
+    
     public void save(Resume r) {
 
         if (getIndex(r.uuid) != -1) {
@@ -42,26 +37,13 @@ public class ArrayStorage {
 
     }
 
-    private int getIndex(String uuid) {
-        for (int i = 0; i < counter; i++) {
-            if ((storage[i].uuid).equals(uuid)) {
-                return i;
-
-// метод getIndex сравнивает переданное uuid каждого элемента массива storage и возвращает целочисленный индекс найденного элемента
-            }
-        }
-        return -1;
-    }
-
     public Resume get(String uuid) {
 
         int index = getIndex(uuid);
         if (index != -1) {
             return storage[index];
-            // int index = getIndex(uuid);
-            //  return storage[index];
         } else {
-            System.out.println("Ошибка");
+            System.out.println("Ошибка поиска резюме для вызова");
             return null;
         }
     }
@@ -70,12 +52,14 @@ public class ArrayStorage {
         int index = getIndex(uuid);
         if (index != -1) {
             counter = counter - 1;
-            storage[index] = storage[counter];
+            if(counter ==0) {
+                storage[index]= null;
+            }else{
+                storage[index] = storage[counter];
+            }
         }
-        //counter--;
-        //storage[counter] = null;
         else {
-            System.out.println("Ошибка");
+            System.out.println("Ошибка поиска резюме для удаления");
         }
     }
 
@@ -85,6 +69,16 @@ public class ArrayStorage {
 
     public int size() {
         return counter;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if ((storage[i].uuid).equals(uuid)) {
+                return i;
+                // метод getIndex сравнивает переданное uuid каждого элемента массива storage и возвращает целочисленный индекс найденного элемента
+            }
+        }
+        return -1;
     }
 }
 
