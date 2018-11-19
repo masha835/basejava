21aca71b43cf61ae8f95a7924a06529777162ef5
PHP <31829822+masha835@users.noreply.github.com>
2018-11-19 10:36:44 +0300

    Update AbstractArrayStorage.java

--- src/AbstractArrayStorage.java
+++ src/AbstractArrayStorage.java
@@ -12,6 +12,29 @@ protected int size = 0;
 public int size() { 
 return size; 
 } 
+ public void clear() {
+   Array.fill(storage, 0, size, null);
+   size = 0;
+ }
+ public void update(Reasume r) {
+   int index = getIndex(r.getUUid());
+   if(index == -1) {
+     System.out.println("Ошибка выбора резюме для обновления");
+   } else {
+     storage[index] = r;
+   }
+ }
+  
+ public void delete(String uuid) {
+   int index = getIndex(uuid);
+   if(index == -1) {
+     System.out.println("Резюме по " + uuid + "не существует");
+   } else {
+     storage[index] = storage[size -1];
+     storage[size -1] = null;
+     size--;
+   }
+ }
 
 public Resume get(String uuid) { 
 int index = getIndex(uuid); 
